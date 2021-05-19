edde6966c5b7605077f1f9ae227ba53a1213b429
GHERRAZ Salah <50901924+ELCALVO@users.noreply.github.com>
2021-05-19 09:17:53 +0200

    [WIP] Jeu (#1)
    * Implémentation du modèle 
    
    * Added required methodes and classes. Resolved pulled requests.
    
    * Corrected recently made comments.
    
    Co-authored-by: Julien Reig <reig.julien@free.fr>
    Co-authored-by: nishithprg <puranik.2001@gmail.com>

---
+++ src/modele/Point.java
@@ -0,0 +1,36 @@
+package modele;
+
+public class Point {
+    private int l;
+    private int c;
+
+
+    public Point(int l, int c){
+        this.l = l;
+        this.c = c;
+    }
+
+    public int getL(){
+        return l;
+    }
+
+    public int getC(){
+        return c;
+    }
+
+    public void setL(int l){
+        this.l = l;
+    }
+
+    public void setC(int c){
+        this.c = c ;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "l=" + l +
+                ", c=" + c +
+                '}';
+    }
+}
