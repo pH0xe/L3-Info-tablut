0462d03781a4838c1ce3cd38de893539926276aa
Julien Reig <reig.julien@free.fr>
2021-05-23 16:38:11 +0200

    Lecture/Ecriture plateau (#9)
    * Initialisation du plateau a partir d'un fichier texte (configurable)
    
    * Implémentation des serializable (Point, Pion, Joueur)
    
    * Implémentation d'une class reader pour lire les fichiers de sauvegardes
    
    * Implémentation d'une class writer pour écrire les sauvegardes

---
+++ src/global/writer/BoardWriterBinary.java
@@ -0,0 +1,31 @@
+package global.writer;
+
+import global.Configuration;
+import modele.Jeu;
+import modele.Joueur.Couleur;
+import modele.pion.Pion;
+
+import java.io.*;
+import java.util.List;
+
+public class BoardWriterBinary {
+    BufferedOutputStream output;
+
+    public BoardWriterBinary() {
+        output = Configuration.creeFichierSave();
+    }
+
+    public void ecrireJeu(Jeu jeu) throws IOException {
+        if (output == null) return;
+        List<Pion> pions = jeu.getPlateau().getBlancs();
+        pions.addAll(jeu.getPlateau().getNoirs());
+        int joueurCourant = jeu.joueurCourant().getCouleur() == Couleur.BLANC ? 0 : 1;
+        ObjectSaver obj = new ObjectSaver(jeu.getJoueurBlanc(), jeu.getJoueurNoir(), joueurCourant, pions);
+
+        ObjectOutputStream objectStream = new ObjectOutputStream(output);
+        objectStream.writeObject(obj);
+
+        output.flush();
+        output.close();
+    }
+}
--- src/modele/util/Point.java
+++ src/modele/util/Point.java
@@ -1,8 +1,9 @@
 package modele.util;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Point {
+public class Point implements Serializable {
     private int l;
     private int c;
 
